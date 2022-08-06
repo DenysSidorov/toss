a3b7308eaab7946bb58be579d049992a04c6cdaa
DenysSidorov <000scorpions000@gmail.com>
2022-08-06 23:56:38 +0300

    add scss file as a main styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./App";
+// import React from 'react';
+import '../src/sass/common.scss';
+// import ReactDOM from 'react-dom';
+// import App from "./App";
 // import '../public/favicon.ico';
 
-const rootId = document.getElementById("root");
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootId
-);
+// const rootId = document.getElementById("root");
+//
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   rootId
+// );
