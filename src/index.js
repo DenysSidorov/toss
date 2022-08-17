255861b164d0d623d16c566ad7d2949006aa499b
DenysSidorov <000scorpions000@gmail.com>
2022-08-18 00:49:51 +0300

    added examples folder with grids

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-// import React from 'react';
+import React from 'react';
 import '../src/sass/reset.scss';
-import '../src/sass/common.scss';
-// import ReactDOM from 'react-dom';
-// import App from "./App";
-// import '../public/favicon.ico';
 
-// const rootId = document.getElementById("root");
-//
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   rootId
-// );
+import ReactDOM from 'react-dom';
+import App from "./App";
+import '../public/favicon.ico';
+
+const rootId = document.getElementById("root");
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootId
+);
